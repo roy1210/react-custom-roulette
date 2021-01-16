75393b0b6e0c6894bf6411694c3fa774e84d7c06
roy1210 <rswkure@gmail.com>
2021-01-16 20:09:50 +0800

    fix: (demo) make spiner longer time 2

--- src/components/Wheel/index.tsx
+++ src/components/Wheel/index.tsx
@@ -44,10 +44,12 @@ interface Props {
 
 const STARTED_SPINNING = 'started-spinning';
 
-const START_SPINNING_TIME = 2600;
-const CONTINUE_SPINNING_TIME = 750;
+// const START_SPINNING_TIME = 2600;
+// const CONTINUE_SPINNING_TIME = 750;
 // const STOP_SPINNING_TIME = 8000;
-const STOP_SPINNING_TIME = 80000;
+const START_SPINNING_TIME = 2600;
+const CONTINUE_SPINNING_TIME = 75000;
+const STOP_SPINNING_TIME = 8000;
 
 export const Wheel = ({
   mustStartSpinning,
